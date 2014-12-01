6b4dfe16ecf49816f7d84763e5b9a1c18a261205
xclouder <xclouder@qq.com>
2014-12-01 18:35:50 +0800

    Initial Commit

---
+++ OnTheRoad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OnTheRoad
+//
+//  Created by xClouder on 14/12/1.
+//  Copyright (c) 2014年 xClouder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
